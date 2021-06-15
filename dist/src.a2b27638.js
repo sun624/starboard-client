639a4bfd573f68fbc54829f906f7912429cc6558
Ethan <sunyichun624@gmail.com>
2021-06-15 06:19:37 -0700

    static

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -10445,7 +10445,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58893" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64457" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
